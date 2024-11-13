141d13f1263be871b1f6f7e207376e81975e2b84
Hasan <mmhasan78607@gmail.com>
2024-11-13 13:13:27 +0530

    EasyTask

---
+++ 01-starting-project/src/app/user/user.component.ts
@@ -0,0 +1,26 @@
+import { Component, EventEmitter, Input, Output} from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users';
+import { User } from './user.model';
+
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css'
+})
+export class UserComponent {
+  @Input() user!:User;
+  @Input({required:true}) selected!:boolean;
+  @Output() select= new EventEmitter<string>();
+   get imagePath(){
+    return 'assets/users/'+this.user.avatar; 
+   }
+  
+
+  onSelectUser() {
+    this.select.emit(this.user.id)
+
+  }
+}
